fec93fb5100dcca16cda6188357d223d4599208c
Gabe Wilmoth <35712811+GabeWilmoth@users.noreply.github.com>
2020-05-03 14:16:07 -0400

    Groundwork for Reddit Meme of the day
    Added in a Reddit API wrapper "snoowrap" to start getting meme of the day photos to post. Currently you can trigger the hardcoded meme (which is actually fetched from the Reddit site) when typing "!meme" in the discord.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 // require the discord.js module
 const Discord = require('discord.js');
+// dotenv for storing the token
 require('dotenv').config();
+// require snoowrap (A wrapper around the reddit API)
+var snoowrap = require('snoowrap');
 
 const token = process.env.TOKEN;
 
 // create a new Discord client
 const client = new Discord.Client();
+
+
+
+let memeURL;
+// Alternatively, just pass in a username and password for script-type apps.
+const r = new snoowrap({
+    userAgent: process.env.userAgentREDDIT,
+    clientId: process.env.clientIdREDDIT,
+    clientSecret: process.env.clientSecretREDDIT,
+    username: process.env.usernameREDDIT,
+    password: process.env.passwordREDDIT
+  });
+
+//Getting the Subreddit for memes
+r.getSubreddit('memes').getHot().then(Listing =>{
+    console.log(Listing[2].url);
+    memeURL = Listing[2].url;
+});
+
+
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
@@ -17,6 +40,13 @@ client.on('message', message => {
 	if (message.content === '!ping') {
         // send back "Pong." to the channel the message was sent in
         message.channel.send('Pong.');
+    }
+});
+
+client.on('message', message => {
+	if (message.content === '!meme') {
+        // send back a meme to the channel the message was sent in
+        message.channel.send(memeURL);
     }
 });
 
